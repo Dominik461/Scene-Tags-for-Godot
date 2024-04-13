2a776b30af50b7fe3986577a116ee58943d1ffe3
Grobiani <antonhorn@icloud.com>
2024-04-13 15:15:25 +0200

    added the scene tag node

--- addons/scenecounter/scenecounter.gd
+++ addons/scenecounter/scenecounter.gd
@@ -4,9 +4,12 @@ extends EditorPlugin
 
 func _enter_tree():
 	# Initialization of the plugin goes here.
+	add_custom_type("scene_tag", "Node", preload("scene_tag.gd"), preload("icon.png"))
+	
 	pass
 
 
 func _exit_tree():
 	# Clean-up of the plugin goes here.
+	remove_custom_type("scene_tag")
 	pass
